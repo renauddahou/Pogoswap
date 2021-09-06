3ad22ea2ce6385ac5edae085769fcc328efeef68
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2021-09-06 19:39:57 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,12 +61,12 @@ def start(update, context): #команда
                 data['ref'][user] = 0
             data['total'] += 1
             data['id'][user] = data['total']
-            #msg = config['intro'] + '\n[WEBSITE]({})'.format(website) #ИНТРО
-            msg = "Hello!Welcome to the pogoswap airdrop chat bot\n\nTo be eligible for our free token distribution program, you will need to complete a few small tasks\n\nClick on the start button to get started." + '\n[WEBSITE]({})'.format(website) #ИНТРО
+            msg = config['intro'] + '\n[WEBSITE]({})'.format(website) #ИНТРО
             
             #ПРИМЕР "*bold* _italic_ `fixed width font` [link](http://google.com)\.",parse_mode= 'MarkdownV2'
             reply_markup = ReplyKeyboardMarkup(continue_key,resize_keyboard=True) #кнопка продолжить
-            update.message.reply_text(msg, parse_mode= 'MarkdownV2',disable_web_page_preview=True,reply_markup=reply_markup) #----+вывод интр
+            #update.message.reply_text(msg, parse_mode= 'MarkdownV2',disable_web_page_preview=True,reply_markup=reply_markup) #----+вывод интр
+            update.message.reply_text(msg) #----+вывод интр
             data['process'][user] = "Continue"
             json.dump(data,open('users.json','w'))
             json.dump(data,open('users2.json','w'))
