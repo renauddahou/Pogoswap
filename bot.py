fc6439af084d4b83b92078f038bea0bfd4e0f85f
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2021-09-06 20:18:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,8 +86,8 @@ def profile(update, context):
         user = str(update.message.chat.username)
         mail = data['mail'][user]
         twi = data['twitter'][user]
-        eth_addr = data['bep20'][user]
-        msg = 'Your Provided Data:\n\n    Name: {}\n\n    E-mail: {}\n\n    Twitter: {}\n\n    bep20 Address: {}'.format(user,mail,twi,eth_addr)
+        bep20_addr = data['bep20'][user]
+        msg = 'Your Provided Data:\n\n    Name: {}\n\n    E-mail: {}\n\n    Twitter: {}\n\n    bep20 Address: {}'.format(user,mail,twi,bep20_addr)
         reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
         update.message.reply_text(msg,reply_markup=reply_markup)
 
