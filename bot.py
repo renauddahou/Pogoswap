fd3b3d6fb1128c7d5e38c4e72ce49c570e928890
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2021-09-06 07:41:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -190,16 +190,7 @@ def get_file(update, context): #команда
                 d = "{},{},{},{},{},{},{}\n".format(i,u,data['twitter'][u],data['eth'][u],data['mail'][u],refrrd,data['ref'][u])
                 f.write(d)
             f.close()
-            f1 = open('users.json','w')
-            f1.write("id,username,twitter username,eth address,mail,no. of persons referred,referred by\n")
-            for u in data['users']:
-                i = str(data['id'][u])
-                refrrd = 0
-                if i in data['referred']:
-                    refrrd = data['referred'][i]
-                d1 = "{},{},{},{},{},{},{}\n".format(i,u,data['twitter'][u],data['eth'][u],data['mail'][u],refrrd,data['ref'][u])
-                f1.write(d1)
-            f1.close()
+            
             bot = Bot(TOKEN)
             bot.send_document(chat_id=update.message.chat.id, document=open('users.csv','rb'))
 
