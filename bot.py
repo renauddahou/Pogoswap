15e2e3366f8b9de91f023c8cb39b1aedae881d2b
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2021-09-06 18:38:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ def start(update, context): #команда
             data['total'] += 1
             data['id'][user] = data['total']
             #msg = config['intro'] + '\n[WEBSITE]({})'.format(website) #ИНТРО
-            msg = "Welcome to the pogoswap airdrop chat bot\n\nTo be eligible for our free token distribution program, you will need to complete a few small tasks\n\nClick on the start button to get started" + '\n[WEBSITE]({})'.format(website) #ИНТРО
+            msg = c\nClick on the start button to get started" + '\n[WEBSITE]({})'.format(website) #ИНТРО
             #ПРИМЕР "*bold* _italic_ `fixed width font` [link](http://google.com)\.",parse_mode= 'MarkdownV2'
             reply_markup = ReplyKeyboardMarkup(continue_key,resize_keyboard=True) #кнопка продолжить
             update.message.reply_text(msg, parse_mode= 'MarkdownV2',disable_web_page_preview=True,reply_markup=reply_markup) #----+вывод интр
@@ -75,6 +75,7 @@ def start(update, context): #команда
             update.message.reply_text(welcome_msg,reply_markup=reply_markup) #----
 
     else:
+        #msg = '{} \n. I don\'t reply in group, come in private'.format(config['intro'])
         msg = '{} \n. I don\'t reply in group, come in private'.format(config['intro'])
         update.message.reply_text(msg)
 
