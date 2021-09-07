952896798e9ed8fe04f259a29e71601862cb8fd4
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2021-09-07 13:16:24 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -90,7 +90,7 @@ def profile(update, context):
         twi = data['twitter'][user]
         ytb = data['Youtube'][user]
         bep20_addr = data['bep20'][user]
-        msg = 'Your Provided Data:\n\n    Name: {}\n\n    E-mail: {}\n\n    Twitter: {}\n\n    bep20 Address: {}'.format(user,mail,twi,ytb,bep20_addr)
+        msg = 'Your Provided Data:\n\n    Name: {}\n\n    E-mail: {}\n\n    Twitter: {}\n\n  Youtube: {}\n\n  bep20 Address: {}'.format(user,mail,twi,ytb,bep20_addr)
         reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
         update.message.reply_text(msg,reply_markup=reply_markup)
 
